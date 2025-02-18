65f06e3cf773c0e15384d2260dccb3182ff15962
Caden F <cfinke895@west-mec.org>
2025-02-18 08:00:51 -0700

    Create Home.js
    Signed-off-by: Caden F <cfinke895@west-mec.org>

---
+++ components/Home.js
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <section id="home">
+      <div className="top">
+        <h1 className="home-title">
+          <span>I</span><span>T</span> <span>S</span><span>e</span>
+          <span>c</span><span>u</span><span>r</span><span>i</span>
+          <span>t</span><span>y</span>
+        </h1>
+        <h2 className="home-subtitle" id="homeSection_subtitle_anim">
+          Class of 2026
+        </h2>
+      </div>
+      <div className="bottom">
+        <div className="circle-1"></div>
+        <div className="circle-2"></div>
+        <div className="image"></div>
+      </div>
+    </section>
+  );
+}
