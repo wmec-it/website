34a7c20f47be63ac46d10b399f9a8502a202be46
Caden F <cfinke895@west-mec.org>
2025-02-18 08:01:07 -0700

    Create About.js
    Signed-off-by: Caden F <cfinke895@west-mec.org>

---
+++ components/About.js
@@ -0,0 +1,20 @@
+export default function About() {
+  return (
+    <section id="about">
+      <div className="anim" id="aboutSection_anim">
+        <h1 className="about-title" id="about_section_title">About us</h1>
+        <p className="about-text" id="aboutSection_text_anim">
+          This is a two-year program at
+          <span id="school_name"> West-Mec Southwest Campus</span> consisting of numerous real-world
+          certification courses. Students have the opportunity to earn
+          certifications, get hands-on experience, see what it's like to work
+          in a professional environment & get contacts in the professional
+          world during this program. With hands-on work and numerous
+          certification opportunities, students can gain work experience and
+          become more successful later in their careers.. -
+          <span className="h">Daniel Lockerby (Ambassador)</span>
+        </p>
+      </div>
+    </section>
+  );
+}
